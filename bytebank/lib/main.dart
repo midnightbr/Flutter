9ecbddd6b38cb17cc4d7ffec077f9e9d7f77540f
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-27 19:16:11 -0300

    Material Components Criando a tela inicial do app para registrar transferencias bancarias.

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Bem vindo ao Flutter', textDirection: TextDirection.ltr));
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Transferências'),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
+      ),
+    ),
+  ));
 }
