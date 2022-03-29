83b6aaa5428933570afdb37bfbdfe60ac218a7e2
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-29 13:43:52 -0300

    Refatoração Refatorando o código para ficar mais legivel, trabalhando como class e herança. Assim ficara mais facil a leitura e manutenção do código do projeto. Além de ser conhecidas como boas praticas dentro da programação.

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -4,17 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('100,00'),
-              subtitle: Text('100.00'),
-            ),
-          ),
-        ],
-      ),
+      body: ListaTransferencias(),
       appBar: AppBar(title: Text('Transferências'),),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
@@ -22,4 +12,45 @@ void main() {
       ),
     ),
   ));
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget> [
+        ItemTransferencia(Transferencia(100.00, 0001)),
+        ItemTransferencia(Transferencia(250.00, 1001)),
+        ItemTransferencia(Transferencia(1500.00, 2001)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.value.toString()),
+        subtitle: Text(_transferencia.numberAccount.toString()),
+      ),
+    );
+  }
+
+}
+
+class Transferencia {
+  final double value;
+  final int numberAccount;
+
+  Transferencia(this.value, this.numberAccount);
+
 }
