4d4a11ef8a1f8a412d597ae6a68b5677a5b4a183
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-29 13:03:35 -0300

    ListTile and Card Feita a parte visual da lista de transferencias de forma estatica, mostrando a utilização do ListTile e Card juntos para poder criar uma lista com varios objetos abaixo.

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -4,6 +4,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('100,00'),
+              subtitle: Text('100.00'),
+            ),
+          ),
+        ],
+      ),
       appBar: AppBar(title: Text('Transferências'),),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
