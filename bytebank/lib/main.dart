1aaf33e96d9a3cd682923146a647359dc50b31c1
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-03-31 17:30:25 -0300

    Widgets Foi separado e criado classes genericas para poder ficar mais simples e facil a leitura, além de um melhor desempenho. Com a classe editor, não é preciso criar uma quantidade massiva de código no programa, no qual fica mais responsivel e criativo a adição de uma nova transação.

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -16,17 +16,66 @@ class BytebankApp extends StatelessWidget {
   }
 }
 
-
 class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumberAccount =
+      TextEditingController();
+  final TextEditingController _controladorCampoValue = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Criando Transferência'),),
-      body: Text('teste'),
+      appBar: AppBar(
+        title: Text('Criando Transferência'),
+      ),
+      body: Column(
+        children: [
+          Editor(_controladorCampoNumberAccount, 'Número da conta', '0000'),
+          Editor(_controladorCampoValue, 'Valor', '00.00',
+              icon: Icons.monetization_on),
+          ElevatedButton(
+            child: Text('Confirmar'),
+            onPressed: () => _criaTransferencia(),
+          ),
+        ],
+      ),
     );
+  }
+  void _criaTransferencia() {
+    final int? numberAccount =
+    int.tryParse(_controladorCampoNumberAccount.text);
+    final double? value =
+    double.tryParse(_controladorCampoValue.text);
+    if (numberAccount != null && value != null) {
+      Transferencia(value, numberAccount);
+    }
   }
 }
 
+class Editor extends StatelessWidget {
+  final TextEditingController _controller;
+  final String _rotulo;
+  final String _dica;
+  final IconData? icon;
+
+  Editor(this._controller, this._rotulo, this._dica, {this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      // Configuração de margem
+      padding: const EdgeInsets.fromLTRB(16.00, 8.00, 16.00, 8.00),
+      child: TextField(
+        // Utilizado para pegar valores digitados pelo usuario
+        controller: _controller,
+        style: TextStyle(fontSize: 24.00),
+        decoration: InputDecoration(
+            icon: icon != null ? Icon(icon) : null,
+            labelText: _rotulo,
+            hintText: _dica),
+      ),
+    );
+  }
+}
 
 class ListaTransferencias extends StatelessWidget {
   @override
@@ -36,14 +85,14 @@ class ListaTransferencias extends StatelessWidget {
         title: Text('Transferências'),
       ),
       body: Column(
-        children: <Widget> [
+        children: <Widget>[
           ItemTransferencia(Transferencia(100.00, 0001)),
           ItemTransferencia(Transferencia(250.00, 1001)),
           ItemTransferencia(Transferencia(1500.00, 2001)),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
     );
@@ -51,9 +100,7 @@ class ListaTransferencias extends StatelessWidget {
 }
 
 class ItemTransferencia extends StatelessWidget {
-
   final Transferencia _transferencia;
-
 
   ItemTransferencia(this._transferencia);
 
@@ -67,7 +114,6 @@ class ItemTransferencia extends StatelessWidget {
       ),
     );
   }
-
 }
 
 class Transferencia {
@@ -76,4 +122,8 @@ class Transferencia {
 
   Transferencia(this.value, this.numberAccount);
 
+  @override
+  String toString() {
+    return 'Transferencia{value: $value, numberAccount: $numberAccount}';
+  }
 }
