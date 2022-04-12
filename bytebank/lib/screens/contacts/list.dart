b0d8261eefc140a05d616bda46cc864e2a086e9a
Marcos Henrique <80632969+midnightbr@users.noreply.github.com>
2022-04-11 23:17:36 -0300

    Lidando com os estados do snapshot Ao receber o snapshot.connectionState, o switch() exigirá a implementação de todas as situações possíveis. Assim, em cenários nos quais precisemos devolver outra coisa, estaremos cientes e poderemos colocar algo que faça sentido nessa situação (ou até mesmo nada, caso a situação não faça sentido).

--- bytebank/lib/screens/contacts/list.dart
+++ bytebank/lib/screens/contacts/list.dart
@@ -15,43 +15,62 @@ class ContactList extends StatelessWidget {
       ),
       body: FutureBuilder<List<Contact>>(
         initialData: [],
-        future: Future.delayed(Duration(seconds: 0))
-            .then((value) => findAllContacts()),
+        future: findAllContacts(),
         builder: (context, snapshot) {
-            // Recebendo os dados do bd
-            final List<Contact> contacts = snapshot.data as List<Contact>;
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                // Recebendo os itens dentro da lista
-                final Contact contact = contacts[index];
-                // Retornando os itens para a classe contactItem para a construção
-                return _ContactItem(contact);
-              },
-              itemCount: contacts.length,
-            );
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircularProgressIndicator(),
-                Text('Loading')
-              ],
-            ),
-          );
+          switch (snapshot.connectionState) {
+            /** Significa que o Future ainda não foi executado
+           * Nesse caso é adicionado algum widget que quando acionado inicia
+           * a Future para que saia desse estado
+           */
+            case ConnectionState.none:
+              break;
+            // Quando os dados ainda estão sendo carregados
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [CircularProgressIndicator(), Text('Loading')],
+                ),
+              );
+            /**
+           * Esse estado significa que ele tem um dado disponivel, mais a Future
+           * ainda não foi finalizada. Conhecida como strin
+           */
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              // Recebendo os dados do bd
+              final List<Contact> contacts = snapshot.data as List<Contact>;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  // Recebendo os itens dentro da lista
+                  final Contact contact = contacts[index];
+                  // Retornando os itens para a classe contactItem para a construção
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+          }
+          /**
+           * Caso nenhum dos casos do switch sejam executaveis (o que é
+           * provavelmente bastante dificil de ocorrer), é sempre bom deixar
+           * algo para não apresentar a tela vermelha de erro com o usuario.
+           */
+          return Text('Unknown error');
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
               .push(
-            MaterialPageRoute(
-              builder: (context) => ContactForm(),
-            ),
-          )
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
               .then(
                 (newContact) => debugPrint(newContact.toString()),
-          );
+              );
         },
         child: Icon(
           Icons.person_add_alt_1,
